36619d8fe1e48c00f54e974ecb69e839b679e7d2
DANNYPY <olatundedaniel943@gmail.com>
2025-09-19 22:47:54 +0100

    remove safearea from scaffold wrapping only the drawer with SafeArea

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -11,51 +11,49 @@ class WidgetTree extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter'),
-          centerTitle: true,
-          titleSpacing: 0,
-          actions: [
-            ValueListenableBuilder(
-              valueListenable: modeSwitchNotifier,
-              builder: (BuildContext context, bool value, Widget? child) {
-                return IconButton(
-                  onPressed: () {
-                    modeSwitchNotifier.value = !value;
-                  },
-                  icon: modeSwitchNotifier.value == true
-                      ? Icon(Icons.light_mode)
-                      : Icon(Icons.dark_mode),
-                );
-              },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter'),
+        centerTitle: true,
+        titleSpacing: 0,
+        actions: [
+          ValueListenableBuilder(
+            valueListenable: modeSwitchNotifier,
+            builder: (BuildContext context, bool value, Widget? child) {
+              return IconButton(
+                onPressed: () {
+                  modeSwitchNotifier.value = !value;
+                },
+                icon: modeSwitchNotifier.value == true
+                    ? Icon(Icons.dark_mode)
+                    : Icon(Icons.light_mode),
+              );
+            },
+          ),
+        ],
+        backgroundColor: const Color.fromARGB(255, 0, 116, 132),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (BuildContext context, int value, Widget? child) {
+          return myPages.elementAt(value);
+        },
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(child: Text('Options')),
+            ListTile(title: Text("Setting"), leading: Icon(Icons.settings)),
+            ListTile(title: Text("Theme"), leading: Icon(Icons.brightness_2)),
+            ListTile(
+              title: Text("Logout"),
+              leading: Icon(Icons.logout_outlined),
             ),
           ],
-          backgroundColor: const Color.fromARGB(255, 0, 116, 132),
         ),
-        body: ValueListenableBuilder(
-          valueListenable: selectedPageNotifier,
-          builder: (BuildContext context, int value, Widget? child) {
-            return myPages.elementAt(value);
-          },
-        ),
-        drawer: Drawer(
-          child: Column(
-            children: [
-              DrawerHeader(child: Text('Options')),
-              ListTile(title: Text("Setting"), leading: Icon(Icons.settings)),
-              ListTile(title: Text("Theme"), leading: Icon(Icons.brightness_2)),
-              ListTile(
-                title: Text("Logout"),
-                leading: Icon(Icons.logout_outlined),
-              ),
-            ],
-          ),
-        ),
-
-        bottomNavigationBar: const BottomTabWidget(),
       ),
+
+      bottomNavigationBar: const BottomTabWidget(),
     );
   }
 }
