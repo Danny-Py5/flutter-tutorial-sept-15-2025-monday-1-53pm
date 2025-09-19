24e0920246a218492114e18c783993457f3f0dae
DANNYPY <olatundedaniel943@gmail.com>
2025-09-19 16:01:39 +0100

    spliting file -> separated the bottom nav bar to a file

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/bottom_tab_widget.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentTab = 0;
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
       ),
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter'),
+            centerTitle: true,
+            titleSpacing: 0,
 
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  int currentTab = 0;
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter'),
-          centerTitle: true,
-          titleSpacing: 0,
-
-          backgroundColor: const Color.fromARGB(255, 0, 116, 132),
-        ),
-        drawer: Drawer(
-          child: Column(
-            children: [
-              DrawerHeader(child: Text('Options')),
-              ListTile(title: Text("Setting"), leading: Icon(Icons.settings)),
-              ListTile(title: Text("Theme"), leading: Icon(Icons.brightness_2)),
-              ListTile(
-                title: Text("Logout"),
-                leading: Icon(Icons.logout_outlined),
-              ),
-            ],
+            backgroundColor: const Color.fromARGB(255, 0, 116, 132),
           ),
-        ),
-        body: currentTab == 0
-            ? Center(child: Text('Hello, this is the first Tab'))
-            : Center(child: Text('Hello, this is the second Tab')),
-        floatingActionButton: Column(
-          mainAxisSize: MainAxisSize.min,
-          spacing: 8,
-          children: [
-            FloatingActionButton(
-              onPressed: () {},
-              focusColor: Colors.red,
-              child: Icon(Icons.play_arrow),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                DrawerHeader(child: Text('Options')),
+                ListTile(title: Text("Setting"), leading: Icon(Icons.settings)),
+                ListTile(
+                  title: Text("Theme"),
+                  leading: Icon(Icons.brightness_2),
+                ),
+                ListTile(
+                  title: Text("Logout"),
+                  leading: Icon(Icons.logout_outlined),
+                ),
+              ],
             ),
-            FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.download_for_offline_outlined),
-            ),
-          ],
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
-          ],
-          // Tab handler
-          onDestinationSelected: (value) {
-            currentTab = value;
-            setState(() => currentTab);
-          },
-          selectedIndex: currentTab,
+          ),
+
+          bottomNavigationBar: const BottomTabWidget(),
         ),
       ),
     );
