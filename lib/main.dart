d140a41f87a7065c8634e044353c5ef16bfd4246
DANNYPY <olatundedaniel943@gmail.com>
2025-09-19 14:48:46 +0100

    SatefulW for switching tabs

--- lib/main.dart
+++ lib/main.dart
@@ -16,55 +16,76 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter'),
-            centerTitle: true,
-            titleSpacing: 0,
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
 
-            backgroundColor: const Color.fromARGB(255, 0, 116, 132),
-          ),
-          drawer: Drawer(
-            child: Column(
-              children: [
-                DrawerHeader(child: Text('Options')),
-                ListTile(title: Text("Setting"), leading: Icon(Icons.settings)),
-                ListTile(
-                  title: Text("Theme"),
-                  leading: Icon(Icons.brightness_2),
-                ),
-                ListTile(
-                  title: Text("Logout"),
-                  leading: Icon(Icons.logout_outlined),
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: Column(
-            mainAxisSize: MainAxisSize.min,
-            spacing: 8,
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter'),
+          centerTitle: true,
+          titleSpacing: 0,
+
+          backgroundColor: const Color.fromARGB(255, 0, 116, 132),
+        ),
+        drawer: Drawer(
+          child: Column(
             children: [
-              FloatingActionButton(
-                onPressed: () {},
-                focusColor: Colors.red,
-                child: Icon(Icons.play_arrow),
+              DrawerHeader(child: Text('Options')),
+              ListTile(title: Text("Setting"), leading: Icon(Icons.settings)),
+              ListTile(title: Text("Theme"), leading: Icon(Icons.brightness_2)),
+              ListTile(
+                title: Text("Logout"),
+                leading: Icon(Icons.logout_outlined),
               ),
-              FloatingActionButton(
-                onPressed: () {},
-                child: Icon(Icons.download_for_offline_outlined),
-              ),
-            ],
-          ),
-          bottomNavigationBar: NavigationBar(
-            destinations: [
-              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-              NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
             ],
           ),
         ),
+        body: currentTab == 0
+            ? Center(child: Text('Hello, this is the first Tab'))
+            : Center(child: Text('Hello, this is the second Tab')),
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 8,
+          children: [
+            FloatingActionButton(
+              onPressed: () {},
+              focusColor: Colors.red,
+              child: Icon(Icons.play_arrow),
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.download_for_offline_outlined),
+            ),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+          ],
+          // Tab handler
+          onDestinationSelected: (value) {
+            currentTab = value;
+            setState(() => currentTab);
+          },
+          selectedIndex: currentTab,
+        ),
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
