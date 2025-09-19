fb14f487aa2d14916b25e49f9be9d197e214635e
DANNYPY <olatundedaniel943@gmail.com>
2025-09-19 23:21:15 +0100

    added comment on what value notifier used for

--- lib/data/value_notifier.dart
+++ lib/data/value_notifier.dart
@@ -2,3 +2,8 @@ import 'package:flutter/material.dart';
 
 ValueNotifier<int> selectedPageNotifier = ValueNotifier(0);
 ValueNotifier<bool> modeSwitchNotifier = ValueNotifier(true);
+
+
+// ValueNotifier,  -> holds value 
+// ValueListenableBuilder -> update value in the ValueNotifier and 
+// allows other widgets to use if needed
