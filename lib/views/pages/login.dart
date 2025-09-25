0fa2f7ffd3be1448b495b184af3e4d42863c18b0
Olatunde Daniel Fatokun <olatundedaniel943@gmail.com>
2025-09-26 00:46:24 +0100

    layoutBuilder, FractionallySizedBox

--- lib/views/pages/login.dart
+++ lib/views/pages/login.dart
@@ -34,60 +34,78 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(Defaults.padding),
-        child: SingleChildScrollView(
-          child: Column(
-            spacing: 20,
-            children: [
-              HeroWidget(title: widget.title),
-              TextField(
-                controller: controllerEmail,
-                onEditingComplete: () {
-                  setState(() {});
-                },
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(Defaults.borderRadius),
-                  ),
-                ),
-              ),
-              TextField(
-                controller: controllerPw,
-                onEditingComplete: () => setState(() => {}),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(Defaults.borderRadius),
-                  ),
-                  hintText: "Password",
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton(
-                  onPressed: () {
-                    if (controllerEmail.text == confirmedEmail &&
-                        controllerPw.text == confirmedPw) {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return WidgetTree();
-                          },
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(Defaults.padding),
+          child: SingleChildScrollView(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return FractionallySizedBox(
+                  widthFactor: constraints.maxWidth < 376
+                      ? 1.0
+                      : constraints.maxWidth < 412
+                      ? 0.9
+                      : 0.75,
+                  child: Column(
+                    // mainAxisSize: constraints.maxWidth ,
+                    spacing: 20,
+                    children: [
+                      HeroWidget(title: widget.title),
+                      TextField(
+                        controller: controllerEmail,
+                        onEditingComplete: () {
+                          setState(() {});
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(
+                              Defaults.borderRadius,
+                            ),
+                          ),
                         ),
-                        (route) => false,
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Invalid credentials")),
-                      );
-                    }
-                  },
-                  child: Text(widget.title),
-                ),
-              ),
-            ],
+                      ),
+                      TextField(
+                        controller: controllerPw,
+                        onEditingComplete: () => setState(() => {}),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(
+                              Defaults.borderRadius,
+                            ),
+                          ),
+                          hintText: "Password",
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: FilledButton(
+                          onPressed: () {
+                            if (controllerEmail.text == confirmedEmail &&
+                                controllerPw.text == confirmedPw) {
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return WidgetTree();
+                                  },
+                                ),
+                                (route) => false,
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("Invalid credentials")),
+                              );
+                            }
+                          },
+                          child: Text(widget.title),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         ),
       ),
--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/data/constants/defaults.dart';
 import 'package:flutter_application_1/data/value_notifier.dart';
 import 'package:flutter_application_1/views/pages/welcome_page.dart';
 
@@ -15,7 +16,33 @@ class _ProfilePageState extends State<ProfilePage> {
     return SingleChildScrollView(
       child: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(Defaults.paddingSm),
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 40),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color.fromARGB(137, 23, 182, 161),
+              ),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 100.0,
+                    backgroundImage: AssetImage("assets/images/my_img.jpg"),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Daniel Olatunde',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                  ),
+                  Text('The Developer', style: KTextStyle.descriptionTextStyle),
+                ],
+              ),
+            ),
+          ),
           ListTile(
+            leading: Icon(Icons.logout_rounded),
             title: Text("Logout"),
             onTap: () {
               selectedPageNotifier.value = 0;
